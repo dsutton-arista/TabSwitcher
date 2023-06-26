a0d39086853f8e143daab0d1c52bfa42bfaccd19
David Sutton <dsutton@arista.com>
2023-06-26 12:59:50 +0100

    Update to all cycling in either direction

--- background.js
+++ background.js
@@ -1,75 +1,64 @@
 let tabIdHistory = [];
 
-chrome.action.onClicked.addListener((tab) => {
-  // Get tabIdHistory from storage
+function switchTab(next = false) {
   chrome.storage.local.get('tabIdHistory', function(data) {
     let tabIdHistory = data.tabIdHistory || [];
-
-    function switchToNextTab() {
-      if (tabIdHistory.length > 0) {
-        // Get the tab to switch to, which is the oldest in the array
-        let switchToTabId = tabIdHistory[0];
-
-        // Remove oldest tab from the start of the array
-        tabIdHistory.shift();
-
-        chrome.tabs.get(switchToTabId, function(tab) {
-          if (chrome.runtime.lastError) {
-            // This tab no longer exists, try the next one
-            switchToNextTab();
-          } else if (tab) {
-            // Push this tab to the end of array, so we can cycle through the tabs
-            tabIdHistory.push(switchToTabId);
-
-            // Store updated history back to storage
-            chrome.storage.local.set({tabIdHistory: tabIdHistory}, function() {
-              chrome.tabs.update(switchToTabId, {active: true});
-            });
-          }
-        });
+    if (tabIdHistory.length > 0) {
+      let switchToTabId;
+      if (next) {
+        // Get the last tab, move it to the start, and then do it again
+        switchToTabId = tabIdHistory.pop();
+        tabIdHistory.unshift(switchToTabId);
+        switchToTabId = tabIdHistory.pop();
+        tabIdHistory.unshift(switchToTabId);
+      } else {
+        // get the first tab and move it to the end
+        switchToTabId = tabIdHistory.shift();
+        tabIdHistory.push(switchToTabId);
       }
+      chrome.tabs.update(switchToTabId, {active: true}, function() {
+        // save updated tab history only after switching the tab
+        chrome.storage.local.set({tabIdHistory: tabIdHistory});
+      });
     }
-
-    switchToNextTab();
   });
+}
+
+
+chrome.action.onClicked.addListener(function() {
+  switchTab();
+});
+
+chrome.commands.onCommand.addListener(function(command) {
+  if (command === "switch_to_previous_tab") {
+    switchTab();
+  } else if (command === "switch_to_next_tab") {
+    switchTab(true); // true indicates that it should switch to the "next" tab
+  }
 });
 
 
 chrome.tabs.onActivated.addListener(activeInfo => {
   let newTabId = activeInfo.tabId;
-
-  // Get existing history from storage
   chrome.storage.local.get('tabIdHistory', function(data) {
-    let tabIdHistory = data.tabIdHistory || [];
-    let checksRemaining = tabIdHistory.length;
-
-    // Check each tab in the history to see if it still exists
-    for (let i = 0; i < tabIdHistory.length; i++) {
-      chrome.tabs.get(tabIdHistory[i], function(tab) {
-        if (chrome.runtime.lastError) {
-          // This tab no longer exists, remove it from tabIdHistory
-          tabIdHistory.splice(i, 1);
+    tabIdHistory = data.tabIdHistory || [];
+    let existingTabs = [];
+    let checkTabs = tabIdHistory.map(id => new Promise(resolve =>
+      chrome.tabs.get(id, tab => {
+        if (!chrome.runtime.lastError) {
+          existingTabs.push(id);
         }
+        resolve();
+      })
+    ));
 
-        checksRemaining--;
-        if (checksRemaining === 0) {
-          // All checks are done, add the new tab to the history
-
-          // Remove newTabId if it's already in the array
-          tabIdHistory = tabIdHistory.filter(id => id !== newTabId);
-
-          // Add newTabId to the end of the array
-          tabIdHistory.push(newTabId);
-
-          // Ensure tabIdHistory only contains the last three tab IDs
-          while (tabIdHistory.length > 3) {
-            tabIdHistory.shift();
-          }
-
-          // Store updated history back to storage
-          chrome.storage.local.set({tabIdHistory: tabIdHistory});
-        }
-      });
-    }
+    Promise.all(checkTabs).then(() => {
+      existingTabs = existingTabs.filter(id => id !== newTabId);
+      existingTabs.push(newTabId);
+      while (existingTabs.length > 3) {
+        existingTabs.shift();
+      }
+      chrome.storage.local.set({tabIdHistory: existingTabs});
+    });
   });
 });
