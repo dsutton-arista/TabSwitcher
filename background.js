4f99db2ada5fbda22ad0ca7b6c0a26d6b627f309
David Sutton <dsutton@arista.com>
2023-06-28 10:20:42 +0100

    Move the tab validity check
    Previously there was a bug when chrome first started as the tab
    history was from the previous session so all of the tab ids were
    invalid. To avoid this issue we move the tab existence check to the
    chrome.commands.onCommand.addListener() and since we no longer need it
    in the chrome.tabs.onActivated.addListener() we remove it from
    there. The chrome.tabs.onActivated.addListener() can simply add the
    newly activated tab to the history, without worrying about the
    validity of the other tabs in the history.

--- background.js
+++ background.js
@@ -30,13 +30,35 @@ chrome.action.onClicked.addListener(function() {
 });
 
 chrome.commands.onCommand.addListener(function(command) {
-  if (command === "switch_to_previous_tab") {
-    switchTab();
-  } else if (command === "switch_to_next_tab") {
-    switchTab(true); // true indicates that it should switch to the "next" tab
-  } else if (command === "switch_between_current_and_last") {
-    switchBetweenCurrentAndLast();
-  }
+  chrome.storage.local.get(['tabIdHistory', 'numTabs'], function(data) {
+    let tabIdHistory = data.tabIdHistory || [];
+    let checksRemaining = tabIdHistory.length;
+
+    // Check each tab in the history to see if it still exists
+    for (let i = 0; i < tabIdHistory.length; i++) {
+      chrome.tabs.get(tabIdHistory[i], function(tab) {
+        if (chrome.runtime.lastError) {
+          // This tab no longer exists, remove it from tabIdHistory
+          tabIdHistory.splice(i, 1);
+        }
+
+        checksRemaining--;
+        if (checksRemaining === 0) {
+          // All checks are done, update the tabIdHistory in storage
+          chrome.storage.local.set({tabIdHistory: tabIdHistory}, function() {
+            // Now that the tabIdHistory is clean, we can execute the command
+            if (command === "switch_to_previous_tab") {
+              switchTab();
+            } else if (command === "switch_to_next_tab") {
+              switchTab(true); // true indicates that it should switch to the "next" tab
+            } else if (command === "switch_between_current_and_last") {
+              switchBetweenCurrentAndLast();
+            }
+          });
+        }
+      });
+    }
+  });
 });
 
 function switchBetweenCurrentAndLast() {
@@ -59,35 +81,19 @@ chrome.tabs.onActivated.addListener(activeInfo => {
   chrome.storage.local.get(['tabIdHistory', 'numTabs'], function(data) {
     let tabIdHistory = data.tabIdHistory || [];
     let numTabs = data.numTabs || 3;
-    let checksRemaining = tabIdHistory.length;
 
-    // Check each tab in the history to see if it still exists
-    for (let i = 0; i < tabIdHistory.length; i++) {
-      chrome.tabs.get(tabIdHistory[i], function(tab) {
-        if (chrome.runtime.lastError) {
-          // This tab no longer exists, remove it from tabIdHistory
-          tabIdHistory.splice(i, 1);
-        }
+    // Remove newTabId if it's already in the array
+    tabIdHistory = tabIdHistory.filter(id => id !== newTabId);
 
-        checksRemaining--;
-        if (checksRemaining === 0) {
-          // All checks are done, add the new tab to the history
+    // Add newTabId to the end of the array
+    tabIdHistory.push(newTabId);
 
-          // Remove newTabId if it's already in the array
-          tabIdHistory = tabIdHistory.filter(id => id !== newTabId);
-
-          // Add newTabId to the end of the array
-          tabIdHistory.push(newTabId);
-
-          // Ensure tabIdHistory only contains the last numTabs tab IDs
-          while (tabIdHistory.length > numTabs) {
-            tabIdHistory.shift();
-          }
-
-          // Store updated history back to storage
-          chrome.storage.local.set({tabIdHistory: tabIdHistory});
-        }
-      });
+    // Ensure tabIdHistory only contains the last numTabs tab IDs
+    while (tabIdHistory.length > numTabs) {
+      tabIdHistory.shift();
     }
+
+    // Store updated history back to storage
+    chrome.storage.local.set({tabIdHistory: tabIdHistory});
   });
 });
