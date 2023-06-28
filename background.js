c333b3910a42a93aea9549b72ec0a9fef467b71e
David Sutton <dsutton@arista.com>
2023-06-28 10:34:09 +0100

    Refactored background.js
    ChatGPT was asked: Can you see any other ways to simplify the code and
    reduce the number of times we need to access the chrome.tabs API?
    
    Here are a few more improvements we can make:
    
    1. Currently, the switchTab and switchBetweenCurrentAndLast functions
    are fetching the tabIdHistory from the storage, even though we're
    already doing that in the chrome.commands.onCommand.addListener(). We
    can modify these functions to accept the tabIdHistory as a parameter
    to avoid fetching it twice.
    
    2. Similarly, the switchTab function is saving the tabIdHistory to
    storage immediately after switching tabs. We can also do this in the
    chrome.commands.onCommand.addListener(), after the command has been
    executed.
    
    3. It seems that the array.splice() function within the for loop in
    chrome.commands.onCommand.addListener() may lead to an incorrect
    operation as it changes the array during the iteration. It might be
    better to use a filter function to clean the history.

--- background.js
+++ background.js
@@ -1,79 +1,64 @@
 let tabIdHistory = [];
 
-function switchTab(next = false) {
-  chrome.storage.local.get('tabIdHistory', function(data) {
-    let tabIdHistory = data.tabIdHistory || [];
-    if (tabIdHistory.length > 0) {
-      let switchToTabId;
-      if (next === false) {
-        // Get the last tab, move it to the start, and then do it again
-        switchToTabId = tabIdHistory.pop();
-        tabIdHistory.unshift(switchToTabId);
-        switchToTabId = tabIdHistory.pop();
-        tabIdHistory.unshift(switchToTabId);
-      } else {
-        // get the first tab and move it to the end
-        switchToTabId = tabIdHistory.shift();
-        tabIdHistory.push(switchToTabId);
-      }
-      chrome.tabs.update(switchToTabId, {active: true}, function() {
-        // save updated tab history only after switching the tab
-        chrome.storage.local.set({tabIdHistory: tabIdHistory});
-      });
+function switchTab(tabIdHistory, next = false) {
+  if (tabIdHistory.length > 0) {
+    let switchToTabId;
+    if (next === false) {
+      // Get the last tab, move it to the start, and then do it again
+      switchToTabId = tabIdHistory.pop();
+      tabIdHistory.unshift(switchToTabId);
+      switchToTabId = tabIdHistory.pop();
+      tabIdHistory.unshift(switchToTabId);
+    } else {
+      // get the first tab and move it to the end
+      switchToTabId = tabIdHistory.shift();
+      tabIdHistory.push(switchToTabId);
     }
-  });
+    chrome.tabs.update(switchToTabId, {active: true});
+  }
+  return tabIdHistory;
 }
 
+function switchBetweenCurrentAndLast(tabIdHistory) {
+  if (tabIdHistory.length > 1) {
+    // Get the second last tab id
+    let secondLastTabId = tabIdHistory[tabIdHistory.length - 2];
 
-chrome.action.onClicked.addListener(function() {
-  switchTab();
-});
+    // Make the second last tab active
+    chrome.tabs.update(secondLastTabId, {active: true});
+  }
+}
 
 chrome.commands.onCommand.addListener(function(command) {
   chrome.storage.local.get(['tabIdHistory', 'numTabs'], function(data) {
     let tabIdHistory = data.tabIdHistory || [];
     let checksRemaining = tabIdHistory.length;
+    let validTabIds = [];
 
     // Check each tab in the history to see if it still exists
-    for (let i = 0; i < tabIdHistory.length; i++) {
-      chrome.tabs.get(tabIdHistory[i], function(tab) {
-        if (chrome.runtime.lastError) {
-          // This tab no longer exists, remove it from tabIdHistory
-          tabIdHistory.splice(i, 1);
-        }
-
+    tabIdHistory.forEach((tabId, i) => {
+      chrome.tabs.get(tabId, function(tab) {
         checksRemaining--;
+        if (!chrome.runtime.lastError) {
+          // This tab still exists, add it to validTabIds
+          validTabIds.push(tabId);
+        }
         if (checksRemaining === 0) {
-          // All checks are done, update the tabIdHistory in storage
-          chrome.storage.local.set({tabIdHistory: tabIdHistory}, function() {
-            // Now that the tabIdHistory is clean, we can execute the command
-            if (command === "switch_to_previous_tab") {
-              switchTab();
-            } else if (command === "switch_to_next_tab") {
-              switchTab(true); // true indicates that it should switch to the "next" tab
-            } else if (command === "switch_between_current_and_last") {
-              switchBetweenCurrentAndLast();
-            }
-          });
+          // All checks are done
+          if (command === "switch_to_previous_tab") {
+            validTabIds = switchTab(validTabIds);
+          } else if (command === "switch_to_next_tab") {
+            validTabIds = switchTab(validTabIds, true); // true indicates that it should switch to the "next" tab
+          } else if (command === "switch_between_current_and_last") {
+            switchBetweenCurrentAndLast(validTabIds);
+          }
+          // Now that the command is executed, update the tabIdHistory in storage
+          chrome.storage.local.set({tabIdHistory: validTabIds});
         }
       });
-    }
+    });
   });
 });
-
-function switchBetweenCurrentAndLast() {
-  // Get tabIdHistory from storage
-  chrome.storage.local.get('tabIdHistory', function(data) {
-    let tabIdHistory = data.tabIdHistory || [];
-    if (tabIdHistory.length > 1) {
-      // Get the second last tab id
-      let secondLastTabId = tabIdHistory[tabIdHistory.length - 2];
-
-      // Make the second last tab active
-      chrome.tabs.update(secondLastTabId, {active: true});
-    }
-  });
-}
 
 chrome.tabs.onActivated.addListener(activeInfo => {
   let newTabId = activeInfo.tabId;
